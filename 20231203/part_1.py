325a3c41f3d166130fbc23fbb2d331ce717986da
Xiangshi Yin <xiangshi.yin@shopify.com>
2023-12-07 18:00:35 -0500

    day 3 draft

--- 20231203/part_1.py
+++ 20231203/part_1.py
@@ -3,17 +3,21 @@ import re
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-numbers_all = []
+rows_w_numbers = []
 with open("20231203_input_example.txt", "r") as f:
-    for line in f:
+    lines = f.readlines()
+    for idx, line in enumerate(lines):
         numbers = []
         matches = re.finditer(r"(\d+)", line)
         for match in matches:
             numbers.append([
                 match.group(),
+                idx,
                 match.start(),
                 match.end()
             ])
-        numbers_all.extend(numbers)
+        rows_w_numbers.extend(numbers)
+        # print(line)
 
-print(numbers_all)
+print(rows_w_numbers)
+
