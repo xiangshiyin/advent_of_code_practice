aadc173ec22034202766e763d30d1e6ead5882e3
Xiangshi Yin <xiangshi.yin@shopify.com>
2023-12-07 23:01:25 -0500

    part 1 answer

--- 20231203/part_1.py
+++ 20231203/part_1.py
@@ -3,9 +3,10 @@ import re
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-rows_w_numbers = []
-with open("20231203_input_example.txt", "r") as f:
-    lines = f.readlines()
+numbers_found = []
+# with open("20231203_input_example.txt", "r") as f:
+with open("20231203_input.txt", "r") as f:
+    lines = [line.strip() for line in f.readlines()]
     for idx, line in enumerate(lines):
         numbers = []
         matches = re.finditer(r"(\d+)", line)
@@ -16,8 +17,38 @@ with open("20231203_input_example.txt", "r") as f:
                 match.start(),
                 match.end()
             ])
-        rows_w_numbers.extend(numbers)
+        numbers_found.extend(numbers)
         # print(line)
 
-print(rows_w_numbers)
+# print(numbers_found)
+
+# filter qualified numbers
+n_cols = len(lines[0])
+n_rows = len(lines)
+print(f"n_cols: {n_cols}, n_rows: {n_rows}")
+qualified_numbers = []
+disqualified_numbers_by_row = {i:[] for i in range(n_rows)}
+
+for number, row, start, end in numbers_found:
+    above = lines[row - 1][max(0, start-1):min(n_cols, end+1)] if row > 0 else "."
+    below = lines[row + 1][max(0, start-1):min(n_cols, end+1)] if row < n_rows - 1 else "."
+    left = lines[row][start-1] if start > 0 else "."
+    right = lines[row][end] if end < n_cols else "."
+
+    if (
+        above == "." * len(above) and
+        below == "." * len(below) and
+        left == "." and
+        right == "."
+    ):
+        disqualified_numbers_by_row[row].append(number)
+        print(f"{above}\n{left}{number}{right}\n{below}")
+        continue
+    else:
+        qualified_numbers.append(number)
+
+# print(qualified_numbers)
+print(f"Answer: {sum([int(n) for n in qualified_numbers])}")
+print(f"Disqualified numbers by row: {disqualified_numbers_by_row}")
+
 
