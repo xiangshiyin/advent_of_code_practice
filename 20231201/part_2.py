5acd3189b76dde8d4291278c9cc0a60a398c1aa6
Xiangshi Yin <xiangshi.yin@shopify.com>
2023-12-09 17:42:14 -0500

    day 1 part 2 fixed

--- 20231201/part_2.py
+++ 20231201/part_2.py
@@ -25,9 +25,26 @@ sum_of_numbers = 0
 # with open("20231201_input_example.txt", "r") as f:
 with open("20231201_input.txt", "r") as f:
     for idx, line in enumerate(f):
-        numbers_found = re.findall(pattern, line)
-        first = numbers_found[0] if numbers_found[0].isdigit() else str_to_number[numbers_found[0]]
-        last = numbers_found[-1] if numbers_found[-1].isdigit() else str_to_number[numbers_found[-1]]
-        print(first + last)
+        first = ''
+        last = ''
+        temp = ''
+        for idx, char in enumerate(line):
+            if char.isdigit():
+                temp = char
+            elif char.isalpha():
+                if line[idx: idx+3] in str_to_number.keys():
+                    temp = str_to_number[line[idx: idx+3]]
+                elif line[idx: idx+4] in str_to_number.keys():
+                    temp = str_to_number[line[idx: idx+4]]
+                elif line[idx: idx+5] in str_to_number.keys():
+                    temp = str_to_number[line[idx: idx+5]]
+            if first == '':
+                first = temp
+        last = temp
         sum_of_numbers += int(first + last)
+            
+        # numbers_found = re.findall(pattern, line)
+        # first = numbers_found[0] if numbers_found[0].isdigit() else str_to_number[numbers_found[0]]
+        # last = numbers_found[-1] if numbers_found[-1].isdigit() else str_to_number[numbers_found[-1]]
+        # sum_of_numbers += int(first + last)
 print(f"Answer: {sum_of_numbers}")
