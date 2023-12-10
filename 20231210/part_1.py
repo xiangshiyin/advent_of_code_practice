ff51f3740b6f0e23d58a10bda80c22619a2a49b2
Xiangshi Yin <xiangshi.yin@shopify.com>
2023-12-10 17:29:45 -0500

    part 1 finished

--- 20231210/part_1.py
+++ 20231210/part_1.py
@@ -1,86 +1,65 @@
 import os
+from collections import deque
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-symbols = {
-    "|": [[1, 0], [-1, 0]],
-    "-": [[0, 1], [0, -1]],
-    "L": [[1, 1]],
-    "J": [[1, -1]],
-    "7": [[-1, -1]],
-    "F": [[-1, 1]],
-}
-
-with open("20231210_input_example_1.txt", "r") as f:
-    # with open("20231210_input.txt", "r") as f:
+# with open("20231210_input_example_2.txt", "r") as f:
+# with open("20231210_input_example_1.txt", "r") as f:
+with open("20231210_input.txt", "r") as f:
     lines = [line.strip() for line in f.readlines()]
 
 print(f"Number of lines: {len(lines)}")
-
-# build the grids
-n_rows = len(lines)
-n_cols = len(lines[0])
-grids = [[-1] * n_cols for i in range(n_rows)]
-visited = [[0] * n_cols for i in range(n_rows)]
 
 # find S
 start = []
 for line in lines:
     if "S" in line:
-        start.append(line.index("S"))
         start.append(lines.index(line))
+        start.append(line.index("S"))
         break
 
 print(f"Start: {start}")
 
-# traverse the grids
-stack = [start]
-neighbors = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+# build the grids
+n_rows = len(lines)
+n_cols = len(lines[0])
+grids = [['.'] * n_cols for i in range(n_rows)]
+visited = [[0] * n_cols for i in range(n_rows)]
+grids[start[0]][start[1]] = 0
+visited[start[0]][start[1]] = 1
 
-while stack:
-    r_curr, c_curr = stack.pop()
-    print(f"Current: {r_curr}, {c_curr}")
-    for dr, dc in neighbors:
-        r = r_curr + dr
-        c = c_curr + dc
-        if 0 <= r < n_rows and 0 <= c < n_cols and visited[r][c] == 0:
-            if (
-                (
-                    lines[r][c] == "|"
-                    and r > r_curr
-                    and lines[r_curr][c_curr] not in ["J", "L"]
-                )
-                or (
-                    lines[r][c] == "|"
-                    and r < r_curr
-                    and lines[r_curr][c_curr] not in ["7", "F"]
-                )
-                or (
-                    lines[r][c] == "-"
-                    and c > c_curr
-                    and lines[r_curr][c_curr] not in ["|", "7"]
-                )
-                or (
-                    lines[r][c] == "L"
-                    and r > r_curr
-                    and lines[r_curr][c_curr] not in ["-", "J", "L"]
-                )
-                or (
-                    lines[r][c] == "J"
-                    and r > r_curr
-                    and lines[r_curr][c_curr] not in ["-", "J", "L"]
-                )
-                or (
-                    lines[r][c] == "7"
-                    and r < r_curr
-                    and lines[r_curr][c_curr] not in ["-", "7", "F"]
-                )
-                or (
-                    lines[r][c] == "F"
-                    and r < r_curr
-                    and lines[r_curr][c_curr] not in ["-", "7", "F"]
-                )
-            ):
-                grids[r][c] = grids[r_curr][c_curr] + 1
-                stack.append([r, c])
-                visited[r][c] = 1
+# traverse the grids
+queue = deque([start])
+# neighbors = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+moves_to_symbols = {
+    (1, 0): ["|", "L", "J"],
+    (-1, 0): ["|", "7", "F"],
+    (0, 1): ["-", "J", "7"],
+    (0, -1): ["-", "L", "F"]
+}
+moves_qualified = {
+    "|": [(1,0), (-1,0)],
+    "-": [(0,1), (0,-1)],
+    "L": [(0,1), (-1,0)],
+    "J": [(0,-1), (-1,0)],
+    "7": [(0,-1), (1,0)],
+    "F": [(0,1), (1,0)],
+    "S": [(1,0), (-1,0), (0,1), (0,-1)]
+}
+
+farthest = 0
+while queue:
+    r_curr, c_curr = queue.popleft()
+    # print(f"Current: {r_curr}, {c_curr}")
+    for r_move, c_move in moves_to_symbols:
+        r_next = r_curr + r_move
+        c_next = c_curr + c_move
+        if (r_move, c_move) in moves_qualified[lines[r_curr][c_curr]] and 0 <= r_next < n_rows and 0 <= c_next < n_cols:
+            if lines[r_next][c_next] in moves_to_symbols[(r_move, c_move)] and not visited[r_next][c_next]:
+                visited[r_next][c_next] = 1
+                grids[r_next][c_next] = grids[r_curr][c_curr] + 1
+                if grids[r_next][c_next] > farthest:
+                    farthest = grids[r_next][c_next]
+                queue.append([r_next, c_next])
+
+print(f"Farthest: {farthest}")
