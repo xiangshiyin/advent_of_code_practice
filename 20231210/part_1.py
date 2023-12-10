3c1f5971306d0bb24615756432d956ba2ef36c38
Xiangshi Yin <piupiuyin@gmail.com>
2023-12-10 14:44:13 -0500

    draft updatd

--- 20231210/part_1.py
+++ 20231210/part_1.py
@@ -3,34 +3,25 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 symbols = {
-    "|": [[1,0], [-1,0]],
-    "-": [[0,1], [0,-1]],
-    "L": [[1,1]],
-    "J": [[1,-1]],
-    "7": [[-1,-1]],
-    "F": [[-1,1]]
+    "|": [[1, 0], [-1, 0]],
+    "-": [[0, 1], [0, -1]],
+    "L": [[1, 1]],
+    "J": [[1, -1]],
+    "7": [[-1, -1]],
+    "F": [[-1, 1]],
 }
 
 with open("20231210_input_example_1.txt", "r") as f:
-# with open("20231210_input.txt", "r") as f:
+    # with open("20231210_input.txt", "r") as f:
     lines = [line.strip() for line in f.readlines()]
-
-# for line in lines:
-#     print(line)
 
 print(f"Number of lines: {len(lines)}")
 
 # build the grids
 n_rows = len(lines)
 n_cols = len(lines[0])
-grids = [
-    [-1] * n_cols
-    for i in range(n_rows)
-]
-visited = [
-    [0] * n_cols
-    for i in range(n_rows)
-]
+grids = [[-1] * n_cols for i in range(n_rows)]
+visited = [[0] * n_cols for i in range(n_rows)]
 
 # find S
 start = []
@@ -44,12 +35,7 @@ print(f"Start: {start}")
 
 # traverse the grids
 stack = [start]
-neighbors = [
-    [1,0],
-    [-1,0],
-    [0,1],
-    [0,-1]
-]
+neighbors = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
 while stack:
     r_curr, c_curr = stack.pop()
@@ -58,5 +44,43 @@ while stack:
         r = r_curr + dr
         c = c_curr + dc
         if 0 <= r < n_rows and 0 <= c < n_cols and visited[r][c] == 0:
-            if (lines[r][c] == "|" and r != r_curr) or (lines[r][c] == "-" and c != c_curr) or (lines[r][c] == "L" and r > r_curr) or (lines[r][c] == "J" and r > r_curr) or (lines[r][c] == "7" and r < r_curr) or (lines[r][c] == "F" and r < r_curr):
+            if (
+                (
+                    lines[r][c] == "|"
+                    and r > r_curr
+                    and lines[r_curr][c_curr] not in ["J", "L"]
+                )
+                or (
+                    lines[r][c] == "|"
+                    and r < r_curr
+                    and lines[r_curr][c_curr] not in ["7", "F"]
+                )
+                or (
+                    lines[r][c] == "-"
+                    and c > c_curr
+                    and lines[r_curr][c_curr] not in ["|", "7"]
+                )
+                or (
+                    lines[r][c] == "L"
+                    and r > r_curr
+                    and lines[r_curr][c_curr] not in ["-", "J", "L"]
+                )
+                or (
+                    lines[r][c] == "J"
+                    and r > r_curr
+                    and lines[r_curr][c_curr] not in ["-", "J", "L"]
+                )
+                or (
+                    lines[r][c] == "7"
+                    and r < r_curr
+                    and lines[r_curr][c_curr] not in ["-", "7", "F"]
+                )
+                or (
+                    lines[r][c] == "F"
+                    and r < r_curr
+                    and lines[r_curr][c_curr] not in ["-", "7", "F"]
+                )
+            ):
                 grids[r][c] = grids[r_curr][c_curr] + 1
+                stack.append([r, c])
+                visited[r][c] = 1
