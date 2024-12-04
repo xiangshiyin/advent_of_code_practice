fc1d9c9bef5079db8e497a13c4b5391a0343c95f
Xiangshi Yin <xiangshi.yin@shopify.com>
2024-12-04 09:41:25 -0500

    day4 part 2

--- 2024/20241204/part_2.py
+++ 2024/20241204/part_2.py
@@ -4,28 +4,6 @@ import re
 # cd into the current directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# utils
-def rotate_90(rows):
-    return [''.join(row) for row in zip(*rows[::-1])]
-def get_diagonals(rows):
-    """
-    slice the matrix diagonally
-    """
-    diagonals = []
-    for i in range(len(rows)):
-        diagonals.append(''.join(
-            rows[j][i-j]
-            for j in range(i + 1)
-        ))
-    
-    rows_mirrored = rotate_90(rotate_90(rows))
-    for i in range(len(rows_mirrored) - 1):
-        diagonals.append(''.join(
-            rows_mirrored[j][i-j]
-            for j in range(i + 1)
-        ))
-    return diagonals
-
 # 1. Read the input file
 rows = []
 # with open("20241204_input_example.txt", "r") as file:
@@ -33,28 +11,16 @@ with open("20241204_input.txt", "r") as file:
     for line in file:
         rows.append(line.strip())
 
-# for r in get_diagonals(rows):
-#     print(r)
-
-# 2. Find the word "XMAS" in the rows, columns, diagonals, and the reversed rows, columns, and diagonals
+# 2. Use a 3x3 sliding window to find the number of 3x3 squares that contain the pattern "M.S.A.M.S"
 counter = 0
-for row in rows:
-    counter += len(re.findall(r"XMAS", row))
-    counter += len(re.findall(r"SAMX", row))
-# print(counter)
-
-for row in rotate_90(rows):
-    counter += len(re.findall(r"XMAS", row))
-    counter += len(re.findall(r"SAMX", row))
-# print(counter)
-
-for row in get_diagonals(rows):
-    counter += len(re.findall(r"XMAS", row))
-    counter += len(re.findall(r"SAMX", row))
-# print(counter)
-
-for row in get_diagonals(rotate_90(rows)):
-    counter += len(re.findall(r"XMAS", row))
-    counter += len(re.findall(r"SAMX", row))
+for i in range(len(rows) - 2):
+    for j in range(len(rows[0]) - 2):
+        if re.search(r"M.S.A.M.S", ''.join(rows[i+k][j:j+3] for k in range(3))):
+            counter += 1
+        if re.search(r"M.S.A.M.S"[::-1], ''.join(rows[i+k][j:j+3] for k in range(3))):
+            counter += 1
+        if re.search(r"S.S.A.M.M"[::-1], ''.join(rows[i+k][j:j+3] for k in range(3))):
+            counter += 1
+        if re.search(r"M.M.A.S.S"[::-1], ''.join(rows[i+k][j:j+3] for k in range(3))):
+            counter += 1
 print(counter)
-
