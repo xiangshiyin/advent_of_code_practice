da1f4b13c5034b6864e857ab9a5e010a34dcd9ce
Xiangshi Yin <xiangshi.yin@shopify.com>
2024-12-07 18:28:42 -0500

    day 7 part 2

--- 2024/20241207/part_2.py
+++ 2024/20241207/part_2.py
@@ -22,24 +22,35 @@ with open(path, "r") as file:
 # If it does, we stop and count the test input as valid
 # when validating, we will start from the last number and try to go back to the first number
 
+def num_of_digits(number):
+    return len(str(number))
+
 def compute_value(test_input, test_value):
-    if len(test_input) == 0:
-        return test_value == 0
+    # print(test_input, test_value)
     if len(test_input) == 1:
         return test_input[0] == test_value
+    extra_check = False
+    if len(test_input) > 1 and test_value == int(test_value) and num_of_digits(abs(int(test_value))) > num_of_digits(test_input[-1]) and str(int(test_value))[-num_of_digits(test_input[-1]):] == str(test_input[-1]):
+        print("extra_check on: ", test_input, int(test_value))
+        extra_check = compute_value(
+            test_input[:-1],
+            int(str(int(test_value))[:-num_of_digits(test_input[-1])])
+        )
     return (
         compute_value(test_input[:-1], test_value - test_input[-1])
         or compute_value(test_input[:-1], test_value / test_input[-1])
+        or extra_check
     )
 
-# test_input = [10, 19]
-# test_value = 190
+# test_input = [6,8,6,15]
+# test_value = 7290
 # print(compute_value(test_input, test_value))
 
 counter = 0
 sum_of_valid_test_values = 0
 for i in range(len(test_values)):
     if compute_value(test_inputs[i], test_values[i]):
+        print(test_inputs[i], test_values[i])
         counter += 1
         sum_of_valid_test_values += test_values[i]
 print(counter)
