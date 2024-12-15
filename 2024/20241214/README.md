41c9291f57b3a912e3350a519f86ed54911d23e5
Xiangshi Yin <xiangshi.yin@shopify.com>
2024-12-15 10:59:53 -0500

    day 14 part 2 finished

--- 2024/20241214/README.md
+++ 2024/20241214/README.md
@@ -1,1 +1,3 @@
 Source: https://adventofcode.com/2024/day/14
+
+![](christmas_tree.png)
--- 2024/20241214/part_2.py
+++ 2024/20241214/part_2.py
@@ -2,7 +2,6 @@ import os
 import sys
 import time
 import re
-from tqdm import tqdm
 
 # cd into the current directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -18,47 +17,50 @@ with open(path, "r") as file:
         pc, pr, vc, vr = re.match(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)", line).groups()
         bots.append((int(pr), int(pc), int(vr), int(vc)))
 
-# print(bots)
+print(f"Number of bots: {len(bots)}")
 
+# 2. Print the grid of bots' initial positions
 if use_example:
     nrows, ncols = 7, 11
 else:
     nrows, ncols = 103, 101
 
-max_sf = float("-inf")
-max_sf_second = None
-
-for second in tqdm(range(nrows * ncols * 2)):
-    result = []
+def print_grid(bots, nrows, ncols):
+    grid = [["." for _ in range(ncols)] for _ in range(nrows)]
     for pr, pc, vr, vc in bots:
-        result.append(
+        grid[pr][pc] = "#"
+    for row in grid:
+        print("".join(row))
+print_grid(bots, nrows, ncols)
+
+# 3. Check if all bots are in unique positions
+positions = set()
+for pr, pc, vr, vc in bots:
+    positions.add((pr, pc))
+print(f"Number of unique positions: {len(positions)}")
+print(f"Number of bots: {len(bots)}")
+
+# 4. Find the second when all bots are in unique positions
+second_range = nrows * ncols
+for second in range(second_range + 1):
+    positions = set()
+    for pr, pc, vr, vc in bots:
+        positions.add(
             (
                 (pr + second * vr) % nrows,
                 (pc + second * vc) % ncols,
+                0,
+                0,
             )
         )
+    print(f"After {second} seconds, number of positions with bots: {len(positions)}")
+    if len(positions) == len(bots):
+        print(f"All bots are in unique position after {second} seconds")
+        break
 
-    vm = nrows // 2
-    hm = ncols // 2
-
-    sf = 1
-    tl, tr, bl, br = 0, 0, 0, 0
-    for pr, pc in result:
-        if pr < vm and pc < hm:
-            tl += 1
-        elif pr < vm and pc > hm:
-            tr += 1
-        elif pr > vm and pc < hm:
-            bl += 1
-        elif pr > vm and pc > hm:
-            br += 1
-    sf = tl * tr * bl * br
-    if sf > max_sf:
-        max_sf = sf
-        max_sf_second = second
-    print(f"After {second} seconds, sf: {sf}, tl: {tl}, tr: {tr}, bl: {bl}, br: {br}")
-
-print(f"Max sf: {max_sf}, at second: {max_sf_second}")
+# 5. Print the grid of bots' positions after the second when all bots are in unique positions
+print_grid(positions, nrows, ncols)
+print(len(positions))
 
 #####################################################
 end_time = time.time()
