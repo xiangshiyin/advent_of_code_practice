9443a4bb6c787157cb7b14d76a462658b68e25d6
Xiangshi Yin <xiangshi.yin@shopify.com>
2024-12-15 11:18:45 -0500

    day 15 initial setup

--- 2024/20241215/part_1.py
+++ 2024/20241215/part_1.py
@@ -9,11 +9,27 @@ start_time = time.time()
 #####################################################
 # 1. Read the input file
 use_example = '--example' in sys.argv
-path = "20241215_input_example.txt" if use_example else "20241215_input.txt"
+# path = "20241215_input_example.txt" if use_example else "20241215_input.txt"
+initial_pos_path = "20241215_initial_pos_example.txt" if use_example else "20241215_initial_pos.txt"
+moves_path = "20241215_moves_example.txt" if use_example else "20241215_moves.txt"
+
 grid = []
-with open(path, "r") as file:
+with open(initial_pos_path, "r") as file:
     for line in file:
         grid.append(list(line.strip()))
+
+with open(moves_path, "r") as file:
+    moves = file.read().strip().replace("\n", "")
+print(moves)
+
+# 2. Find the initial position of the bot
+br, bc = None, None
+for r, row in enumerate(grid):
+    for c, cell in enumerate(row):
+        if cell == "@":
+            br, bc = r, c
+            break
+print(f"Initial position of the bot: {br}, {bc}")
 
 #####################################################
 end_time = time.time()
