bfe29793eded650d82281129052adabefe99cee9
Xiangshi Yin <xiangshi.yin@shopify.com>
2024-12-15 14:29:44 -0500

    day 15 part 1

--- 2024/20241215/part_1.py
+++ 2024/20241215/part_1.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+from tqdm import tqdm
 
 # cd into the current directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,11 @@ with open(initial_pos_path, "r") as file:
     for line in file:
         grid.append(list(line.strip()))
 
+nrows, ncols = len(grid), len(grid[0])
+print(f"Dimensions of the grid: {nrows}x{ncols}")
+
 with open(moves_path, "r") as file:
     moves = file.read().strip().replace("\n", "")
-print(moves)
 
 # 2. Find the initial position of the bot
 br, bc = None, None
@@ -30,6 +33,58 @@ for r, row in enumerate(grid):
             br, bc = r, c
             break
 print(f"Initial position of the bot: {br}, {bc}")
+
+# 3. Find out the number of boxes and walls
+boxes, walls = 0, 0
+for row in grid:
+    for cell in row:
+        if cell == "O":
+            boxes += 1
+        elif cell == "#":
+            walls += 1
+print(f"Number of boxes: {boxes}, number of walls: {walls}, number of grid cells: {nrows * ncols}")
+
+# 5. Simulate the moves
+def print_grid(grid):
+    for row in grid:
+        print(*row, sep="")
+
+for idx, move in enumerate(tqdm(moves)):
+    dr = {"v": 1, "^": -1}.get(move, 0)
+    dc = {">": 1, "<": -1}.get(move, 0)
+    # print(f"Move {idx + 1}: {dr}, {dc} || Bot: {br}, {bc}")
+    targets = [(br, bc)]
+    cbr, cbc = br, bc
+    go = True
+    while True:
+        cbr, cbc = cbr + dr, cbc + dc
+        if grid[cbr][cbc] == "#":
+            go = False
+            break
+        if grid[cbr][cbc] == "O":
+            targets.append((cbr, cbc))
+        if grid[cbr][cbc] == ".":
+            break
+    if not go: continue
+    grid[br][bc] = "."
+    grid[br + dr][bc + dc] = "@"
+    for r, c in targets[1:]:
+        grid[r + dr][c + dc] = "O"
+    br, bc = br + dr, bc + dc
+    # print(f"After move {idx + 1}:")
+    # print_grid(grid)
+
+# 6. Calculate the answer
+print(
+    sum(
+        r * 100 + c
+        for r in range(nrows)
+        for c in range(ncols)
+        if grid[r][c] == "O"
+    )
+)
+
+
 
 #####################################################
 end_time = time.time()
