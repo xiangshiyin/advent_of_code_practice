b4c9caa4601560273086700987ee75a2fe96ec7a
Xiangshi Yin <xiangshi.yin@shopify.com>
2024-12-19 23:35:17 -0500

    day 20 template

---
+++ 2024/20241220/part_1.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import time
+
+# cd into the current directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+start_time = time.time()
+#####################################################
+# 1. Read the input file
+use_example = '--example' in sys.argv
+path = "20241220_input_example.txt" if use_example else "20241220_input.txt"
+grid = []
+with open(path, "r") as file:
+    for line in file:
+        grid.append(list(line.strip()))
+
+#####################################################
+end_time = time.time()
+print(f"Time taken: {end_time - start_time} seconds")
